ed4e0efb1f358be9e68197eabb233ef393f3dbe6
Łukasz Kurdziel <lukasz.kurdziel99@gmail.com>
2024-08-09 18:22:05 +0200

    test onuserclick fetch

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
 import './styles/main.scss'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeSAAAcript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+document.querySelector<HTMLButtonElement>('#resBtn')?.addEventListener('click', () => {
+  console.log('resBtn clicked')
+  fetch('./src/main.html')
+    .then(response => response.text())
+    .then(data => {
+      const app = document.getElementById('app');
+      if (app) {
+        app.innerHTML = data;
+      }
+    });
+})
